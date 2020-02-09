737511f0181d2b3f0bbdba2d7dc8a49d44385d06
Arsène Lapostolet <arsene@lapostolet.fr>
2020-02-09 14:09:17 +0100

    initial commit

---
+++ src/main/java/fr/arsene/alkoholspiele/AlkoholspieleApplication.java
@@ -0,0 +1,13 @@
+package fr.arsene.alkoholspiele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkoholspieleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkoholspieleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/arsene/alkoholspiele/AlkoholspieleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arsene.alkoholspiele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkoholspieleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
