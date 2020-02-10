f51515d8567dca81fabec0df09bb01567dfb5c59
Arsène Lapostolet <arsene@lapostolet.fr>
2020-02-10 14:54:14 +0100

    getting rid of dto layer

--- src/main/java/fr/arsene/alkoholspiele/controller/GameController.java
+++ src/main/java/fr/arsene/alkoholspiele/controller/GameController.java
@@ -1,9 +1,5 @@
 package fr.arsene.alkoholspiele.controller;
 
-import fr.arsene.alkoholspiele.controller.dtos.GameDTO;
-import fr.arsene.alkoholspiele.controller.dtos.JokeDTO;
-import fr.arsene.alkoholspiele.controller.dtos.mapper.GameMapper;
-import fr.arsene.alkoholspiele.controller.dtos.mapper.JokeMapper;
 import fr.arsene.alkoholspiele.model.documents.Game;
 import fr.arsene.alkoholspiele.model.documents.Joke;
 import fr.arsene.alkoholspiele.model.repositories.GameRepository;
@@ -15,13 +11,8 @@ import reactor.core.publisher.Mono;
 public class GameController {
 
     private GameRepository gameRepository;
-    private GameMapper gameMapper;
-    private JokeMapper jokeMapper;
-
-    public GameController(GameRepository gameRepository, GameMapper gameMapper, JokeMapper jokeMapper) {
+    public GameController(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
-        this.gameMapper = gameMapper;
-        this.jokeMapper = jokeMapper;
     }
 
 
@@ -29,28 +20,24 @@ public class GameController {
     @PostMapping("/")
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
-    public GameDTO createGame(@RequestBody GameDTO dto){
-        Game game = gameMapper.dtoToGame(dto);
-        this.gameRepository.save(game);
-        return gameMapper.gameToDto(game);
+    public Mono<Game> createGame(@RequestBody Game game){
+        return this.gameRepository.save(game);
     }
 
     // Get a Game from id
     @GetMapping("/{gameId}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public Mono<GameDTO> getGamebyId(@PathVariable("gameId") String gameId){
-        return this.gameRepository.findById(gameId).map(e -> gameMapper.gameToDto(e));
+    public Mono<Game> getGamebyId(@PathVariable("gameId") String gameId){
+        return this.gameRepository.findById(gameId);
     }
 
     // Add Joke to a game from its id
     @PostMapping("/{gameId}/jokes")
-    public JokeDTO addJoke(@PathVariable("gameId") String gameId, @RequestBody JokeDTO dto){
+    public Mono<Game> addJoke(@PathVariable("gameId") String gameId, @RequestBody Joke joke){
         Game game = this.gameRepository.findById(gameId).block();
-        Joke joke = jokeMapper.dtoToJoke(dto);
         game.getJokes().add(joke);
-        this.gameRepository.save(game);
-        return jokeMapper.jokeToDto(joke);
+        return this.gameRepository.save(game);
     }
 
 }
